994ca8f3b4aa1dd88aafd4532e04641273e80a66
Hebert <hebertphp@gmail.com>
2022-10-26 20:18:07 -0300

    26/10/22

--- aula5/aula5_par_impar.php
+++ aula5/aula5_par_impar.php
@@ -3,10 +3,12 @@ if(count($_POST)==0){
     echo "Execute primeiro o formulário";
     exit();//saia do código
 }
-$num=$_POST["num"];
-$ra=$_POST["ra"];
+$num=$_POST["num"];//resgatando o dado do input, name="num"
+$ra=$_POST["ra"];//resgatando o dado do input, name="ra"
+$nome=$_POST["nome"];//resgatando o dado do input, name="nome"
 
 echo "<h2>Seu Ra: $ra</h2>";
+echo "<h2>Nome: $nome</h2>";
 // Código Par Impar com Ternário
 $msg=($num%2==0)?"PAR":"ÍMPAR";
 echo "$num - $msg<hr>";
