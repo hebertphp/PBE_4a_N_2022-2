05ed94e97fda282390f8cae7b364b7b823d68b06
Hebert <hebertphp@gmail.com>
2022-11-09 21:40:36 -0300

    09/11/22

--- aula5/aula5_06-1.php
+++ aula5/aula5_06-1.php
@@ -1,10 +1,15 @@
 <?php
 echo "<h2>do while</h2>";
-$i=0; // inicialização do nosso contador;
+$i=0; 
 do {
-    echo $i . "<br> ";
-    $i++; // incremento do contador
+    echo $i . " ";
+    $i++;
 } while ($i < 5);
-
+echo "<h2>while</h2>";
+$i=0;
+while($i < 5){
+    echo $i." ";
+    $i++;
+}
 
 ?>
