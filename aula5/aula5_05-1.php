8f49549f28faba6bdedd498f6487e97767cc84bf
Hebert <hebertphp@gmail.com>
2022-11-09 21:01:49 -0300

    09/11/22

--- aula5/aula5_05-1.php
+++ aula5/aula5_05-1.php
@@ -1,7 +1,30 @@
 <?php
-$i=0; // inicialização do contador;
-while ($i <= 5) {
- echo $i . "<br> ";
- $i++;
+$i=0; // valor inicial
+while ($i <= 5) {//condição
+ echo $i . " ";
+ $i++;//contador
 }
+echo "<hr>";
+/*
+$i      $i<=5       Saída       Contador($i++)
+0       V           0           1
+1       V           1           2
+2       V           2           3
+3       V           3           4
+4       V           4           5
+5       V           5           6
+6       F
+*/
+
+/*
+Desafio 1: Mostrar a saída abaixo
+
+7 8 9 10
+*/
+$i=0; // valor inicial
+while ($i <= 5) {//condição
+ echo $i . " ";
+ $i++;//contador
+}
+echo "<hr>";
 ?>
