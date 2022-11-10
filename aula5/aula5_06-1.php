0643135226f4ce03115262cc447aa3f365c8cf39
Hebert <hebertphp@gmail.com>
2022-11-09 21:50:08 -0300

    09/11/22

--- aula5/aula5_06-1.php
+++ aula5/aula5_06-1.php
@@ -1,15 +1,14 @@
 <?php
 echo "<h2>do while</h2>";
-$i=0; 
+$i=10; 
 do {
     echo $i . " ";
     $i++;
 } while ($i < 5);
 echo "<h2>while</h2>";
-$i=0;
+$i=10;
 while($i < 5){
     echo $i." ";
     $i++;
 }
-
 ?>
