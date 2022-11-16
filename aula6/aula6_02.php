949abe7240d70c1da80a7f858a9d378878302675
Hebert <hebertphp@gmail.com>
2022-11-16 20:49:06 -0300

    16/11/22

--- aula6/aula6_02.php
+++ aula6/aula6_02.php
@@ -1,6 +1,8 @@
 <?php
+//                0         1         2
 $cliente=array("Fulano","Beltrano","Sicrano");
 echo "<br>".$cliente[0];
 echo "<br>".$cliente[1];
 echo "<br>".$cliente[2];
+echo "<hr><pre>";print_r($cliente);echo "</pre>";
 ?>
