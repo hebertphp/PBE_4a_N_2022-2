ed4ff5ce171c1502cf6cff28a511fb0b7b0efe0f
Hebert <hebertphp@gmail.com>
2022-11-16 20:38:01 -0300

    16/11/22

--- aula6/aula6_01.php
+++ aula6/aula6_01.php
@@ -7,6 +7,6 @@ $i=40;
 */
 //posição   0  1  2  3  4   5   6   7 
    $i=array(10,20,30,40,120,290,380,2022);
-//print_r($i);//imprime todos os valores da var do tipo array
+print_r($i);//imprime todos os valores da var do tipo array
 echo $i[6];//380
 ?>
