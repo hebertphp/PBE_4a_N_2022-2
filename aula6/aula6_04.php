207f18c70170639aed527774f261196c3ac68855
Hebert <hebertphp@gmail.com>
2022-11-16 21:29:58 -0300

    16/11/22

--- aula6/aula6_04.php
+++ aula6/aula6_04.php
@@ -1,8 +1,8 @@
 <?php
-$aluno=array();
 $aluno["curso"] = "TADS";
 $aluno["nome"] = "Fulano";
-$aluno["ra"] = 123456789;
+$aluno["ra"] = 1234056789;
+//$aluno=array();//cria uma var do tipo array vazia, apagar todas as posições e valores do array
 echo "<pre>";print_r($aluno);echo "</pre>";
 echo "<hr>Nome: ".$aluno["nome"];
 echo "<br>Ra: ".$aluno["ra"];
