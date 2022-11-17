ed2881b25c81e3fa085e50eab3dfd093fc208759
Hebert <hebertphp@gmail.com>
2022-11-16 21:03:57 -0300

    16/11/22

--- aula6/aula6_03.php
+++ aula6/aula6_03.php
@@ -1,8 +1,15 @@
 <?php
-$usuario[]="Roberta";
-$usuario[]="Carlos";
-$usuario[]="Marilda";
+$usuario[]="Roberta";//0
+$usuario[]="Carlos";//1
+$usuario[]="Marilda";//2
+$usuario[]=10;//3
+$usuario[]=9.5;//4
+$usuario[]=true;//5
 echo "<br>".$usuario[0];
 echo "<br>".$usuario[1];
 echo "<br>".$usuario[2];
+echo "<br>".$usuario[3];
+echo "<br>".$usuario[4];
+echo "<br>".$usuario[5];
+echo "<hr><pre>";print_r($usuario);echo "</pre>";
 ?>
