30187adfcdb38bc816d5c4e5f84ec596fbe3c0f3
Hebert <hebertphp@gmail.com>
2022-11-23 20:16:20 -0300

    23/11/22

--- aula6/aula6_07.php
+++ aula6/aula6_07.php
@@ -1,7 +1,17 @@
 <?php
 $nota=9.5;
 $nota=7;
+//            0    1   2
 $notas=array(9.5 , 7 , 8);
 echo $nota."<br>";
-echo "<pre>";print_r($notas);echo "</pre>";
+echo "<pre>";print_r($notas);echo "</pre><hr>";
+echo $notas[2]. " - ".$notas[0];
+$estudante=array(
+    "ra"  => 123456789,
+    "nome"=>"Pafuncia",
+    "av1" =>9,
+    "av2" =>7
+);
+echo "<pre>";print_r($estudante);echo "</pre><hr>";
+
 ?>
